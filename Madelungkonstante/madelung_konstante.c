bc2f6e34d4fdb76781fd2313a115de461e0a7101
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2014-04-22 21:06:11 +0200

    3D und 2D in eine Quelldatei zusammengefügt

--- Madelungkonstante/madelung_konstante.c
+++ Madelungkonstante/madelung_konstante.c
@@ -6,7 +6,12 @@
 
 /* Berechnet die Madelungkonstante für einen Würfel der Kantenlänge 2n wobei die Länge 
    auf den Gitterabstand normalisiert ist */
-double evjens_method(int n);
+double madelung_3d(int n);
+
+/* Berechnet die Madelungkonstante für ein Quadrat der Seitenlänge 2n wobei die Länge
+   auf den Gitterabstand normalisiert ist */
+double madelung_2d(int n);
+
 
 /* Beschreibung was eigentlich in der main-Funktion berechnet wird */
 int main() {
@@ -15,20 +20,23 @@ int main() {
   double mad;
 
   for (int i = 1; i < 10; i++) {
-    mad = -evjens_method(i);
+    mad = -madelung_3d(i);
     printf("Madelung-Konstante: %.12f \t n: %i \t Delta_rel: %.2E\n", mad, i,
            fabs(mad - lit) / lit);
   }
   
   for (int i = 1; i < 10; i++) {
-    mad = -evjens_method(10 * i);
+    mad = -madelung_3d(10 * i);
     printf("Madelung-Konstante: %.12f \t n: %i \t Delta_rel: %.2E\n", mad, 10 * i,
            fabs(mad - lit) / lit);
   }
+  
+  printf("Madelungkonstante: %f\n", -madelung_2d(100));
+  
   return 0;
 }
 
-double evjens_method(int n) {
+double madelung_3d(int n) {
   double sum = 0;
   double rest = 0;
   
@@ -60,6 +68,37 @@ double evjens_method(int n) {
     /* Ecken * 8 wegen Symmetrie (welche Ecke wird berechnet?)*/
     sum += pow(-1, m + m + m) / sqrt(m * m + m * m + m * m);
     rest += 7 * pow(-1, m + m + m) / sqrt(m * m + m * m + m * m);
+  }
+
+  return sum;
+}
+
+
+double madelung_2d(int n) {
+  double sum = 0;
+  double rest = 0;
+
+  /* effizient der pow-Funktion diskutieren (könnte durch eine Modulo Operation
+     eliminiert werden
+     macht aber den Code unklarer */
+  for (int m = 1; m <= n; m++) {
+    /* Den Rest vom letzten Durchgang aufaddieren und danach resetten */
+    sum += rest;
+    rest = 0;
+
+    /*
+    magic numbers müssen noch entfernt bzw. erklärt werden
+    */
+
+    /* Kante  * 4 wegen Symmetrie (welche Kante wird berechnet?) */
+    for (int x = -m + 1; x < m; x++) {
+      sum += 2 * pow(-1, x + m) / sqrt(x * x + m * m);
+      rest += 2 * pow(-1, x + m) / sqrt(x * x + m * m);
+    }
+
+    /* Ecken * 4 wegen Symmetrie (welche Ecke wird berechnet?)*/
+    sum += pow(-1, m + m) / sqrt(m * m + m * m);
+    rest += 3 * pow(-1, m + m) / sqrt(m * m + m * m);
   }
 
   return sum;
