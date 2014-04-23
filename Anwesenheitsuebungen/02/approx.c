bc6946a82203f913a3fcfd79d573c9b2fe30fc46
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2014-04-23 10:52:43 +0200

    Basis

--- Anwesenheitsuebungen/02/approx.c
+++ Anwesenheitsuebungen/02/approx.c
@@ -9,5 +9,19 @@ typedef struct {
 double *newton_coeff(tuple *stuetz, int n);
 
 int main() {
+  tuple *s = NULL;
+  int count = 0;
+  double *coeff = NULL;
+  
+  coeff = newton_coeff(s, count);
+
+  free(coeff);
   return 0;
+}
+
+double *newton_coeff(tuple *stuetz, int n) {
+  double *ret;
+  ret = malloc(n * sizeof(tuple));
+
+  return ret;
 }
