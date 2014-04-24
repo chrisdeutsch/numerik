5689e6b5df376acef11a427b7af28962319894ee
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2014-04-24 14:25:14 +0200

    Magic Numbers erklärt

--- Madelungkonstante/madelung_konstante.c
+++ Madelungkonstante/madelung_konstante.c
@@ -7,7 +7,7 @@
 /* Literaturwert nach Y. Sakamoto */
 #define MAD_CONST_3D 1.7475645946331822
 /* Hier noch eine vernünftige Quelle */
-#define MAD_CONST_2D 1.6155
+#define MAD_CONST_2D 1.61554
 
 /* Berechnet die Madelungkonstante für einen Würfel der Kantenlänge 2n wobei die Länge 
    auf den Gitterabstand normalisiert ist */
@@ -26,7 +26,7 @@ int sign_z(int x, int y, int z);
 
 /* Beschreibung was eigentlich in der main-Funktion berechnet wird */
 int main() {
-  printf("Output:\n");  
+  printf("Output:\n");
   return 0;
 }
 
@@ -34,18 +34,15 @@ double madelung_3d(int n) {
   double sum = 0; //wo ist der Unterschied zwischen sum und rest?
   double rest = 0;
   
-  /* effizient der pow-Funktion diskutieren (könnte durch eine Modulo Operation eliminiert werden
-     macht aber den Code unklarer */
   for (int m = 1; m <= n; m++) { //Madelungkonst von Würfel mit Kantenlänge 2 + Madelungkonst von Würfel mit Kantenlänge 4 + ...?
     /* Den Rest vom letzten Durchgang aufaddieren und danach resetten */
     sum += rest;
     rest = 0;
 
-    /*
-    magic numbers müssen noch entfernt bzw. erklärt werden
+    /* Fläche (welche Fläche wird berechnet?)
+       sum: 6 (Sym.) * 1/2 (Evjens) = 3
+       rest: 6 (Sym.) * 1/2 (Rest) = 3
     */
-
-    /* Fläche  * 6 wegen Symmetrie (welche Fläche wird berechnet?) */
     for (int y = -m + 1; y < m; y++) {
       for (int x = -m + 1; x < m; x++) {
         sum += 3 * sign_z(x, y, m) / dist(x, y, m);
@@ -53,13 +50,19 @@ double madelung_3d(int n) {
       }
     }
 
-    /* Kanten * 12 wegen Symmetrie (welche Kante wird berechnet?) */
+    /* Kanten (welche Kante wird berechnet?)
+       sum: 12 (Sym.) * 1/4 (Evjens) = 3
+       rest: 12 (Sym.) * 3/4 (Rest) = 9
+    */
     for (int x = -m + 1; x < m; x++) {
       sum += 3 * sign_z(x, m, m) / dist(x, m, m);
       rest += 9 * sign_z(x, m, m) / dist(x, m, m);
     }
 
-    /* Ecken * 8 wegen Symmetrie (welche Ecke wird berechnet?)*/
+    /* Ecken (welche Ecke wird berechnet?)
+       sum: 8 (Sym.) * 1/8 (Evjens) = 1
+       rest: 8 (Sym.) * 7/8 (Rest) = 7
+    */
     sum += sign_z(m, m, m) / dist(m, m, m);
     rest += 7 * sign_z(m, m, m) / dist(m, m, m);
   }
@@ -72,25 +75,24 @@ double madelung_2d(int n) {
   double sum = 0;
   double rest = 0;
 
-  /* effizient der pow-Funktion diskutieren (könnte durch eine Modulo Operation
-     eliminiert werden
-     macht aber den Code unklarer */
   for (int m = 1; m <= n; m++) {
     /* Den Rest vom letzten Durchgang aufaddieren und danach resetten */
     sum += rest;
     rest = 0;
 
-    /*
-    magic numbers müssen noch entfernt bzw. erklärt werden
+    /* Kante (welche Kante wird berechnet?)
+       sum: 4 (Sym.) * 1/2 (Evjens) = 2
+       rest: 4 (Sym.) * 1/2 (Rest) = 2
     */
-
-    /* Kante  * 4 wegen Symmetrie (welche Kante wird berechnet?) */
     for (int x = -m + 1; x < m; x++) { //War das nicht Kanten 1/2 gewichtet?
       sum += 2 * sign_z(x, m, 0) / dist(x, m, 0);
       rest += 2 * sign_z(x, m, 0) / dist(x, m, 0);
     }
 
-    /* Ecken * 4 wegen Symmetrie (welche Ecke wird berechnet?)*/
+    /* Ecken * 4 wegen Symmetrie (welche Ecke wird berechnet?)
+       sum: 4 (Sym.) * 1/4 (Evjens) = 1
+       rest: 4 (Sym.) * 3/4 (Rest) = 3    
+    */
     sum += sign_z(m, m, 0) / dist(m, m, 0);
     rest += 3 * sign_z(m, m, 0) / dist(m, m, 0);
   }
@@ -102,6 +104,8 @@ double dist(int x, int y, int z) {
   return sqrt(x*x + y*y + z*z);
 }
 
+/* effizient der pow-Funktion diskutieren (könnte durch eine Modulo Operation eliminiert werden
+   macht aber den Code unklarer */
 int sign_z(int x, int y, int z) {
   return -pow(-1, x + y + z);
 }
