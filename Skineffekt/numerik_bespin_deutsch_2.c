846e8e8fa72daf27a9665304e7b12ea348955d57
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2014-04-29 12:41:55 +0200

    Bessel Imag

--- Skineffekt/numerik_bespin_deutsch_2.c
+++ Skineffekt/numerik_bespin_deutsch_2.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
 double ber(double x);
 double bei(double x);
 
-int main(){
+int main() {
   printf("Hallo Welt\n");
-  
+
   return 0;
+}
+
+double bei(double x) {
+  const double xhalf = x / 2;
+  
+  double sum = xhalf * xhalf;
+  double summand = xhalf * xhalf;
+
+  double epsilon = 1E-6;
+  int i = 3;
+
+  while (fabs(summand) > epsilon) {
+    summand *= -xhalf * xhalf * xhalf * xhalf / ((i - 1) * (i - 1) * i * i);
+    i += 2;
+    sum += summand;
+  }
+
+  return sum;
 }
