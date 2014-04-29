298281d8ee74ba534b25f89a3c97a00d5f79742d
Christian Bespin <ljchriss@gmail.com>
2014-04-29 12:43:06 +0200

    ber

--- Skineffekt/numerik_bespin_deutsch_2.c
+++ Skineffekt/numerik_bespin_deutsch_2.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 double ber(double x);
 double bei(double x);
 
-int main(){
+int main() {
   printf("Hallo Welt\n");
-  
+  printf("%f \n", ber(2));
+
   return 0;
+}
+
+double ber(double x) {
+  double epsilon = 1E-6;
+  double summe = 1;
+  double a = -pow(x, 4) / 4;
+
+  for (int k = 2; fabs(a) > epsilon; k++) {
+    summe += a;
+    a *= -pow(x, 4) / 16 * 1 / (pow((2 * k + 2), 2) * pow((2 * k + 1), 2));
+  }
+  
+  return summe;
 }
