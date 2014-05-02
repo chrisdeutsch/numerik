cbcdea82ced04a0b628a631ac541f2327dd64ac8
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2014-05-02 13:02:06 +0200

    asymptotische näherung für ber

--- Skineffekt/numerik_bespin_deutsch_2.c
+++ Skineffekt/numerik_bespin_deutsch_2.c
@@ -12,39 +12,45 @@ double g1(double x);
 int main() {
   printf("ber(100) = %.10E\n", ber(100));
   printf("bei(100) = %.10E\n", bei(100));
-  
+
   printf("f(100) = %.5E\n", f1(1000));
   printf("g(100) = %.5E\n", g1(1000));
-  
+
   return 0;
 }
 
 double ber(double x) {
-   const double factor = pow(x,4)/16;
+  if (fabs(x) <= 100) {
+    const double factor = pow(x, 4) / 16;
 
-   double sum = 1;
-   double summand = 1;
+    double sum = 1;
+    double summand = 1;
 
-   double epsilon = 1E-6;
-   int i = 2;
+    double epsilon = 1E-6;
+    int i = 2;
 
-   while (fabs(summand) > epsilon) {
-     summand *= -factor / ((i - 1) * (i - 1) * i * i);
-     i += 2;
-     sum += summand;
-   }
+    while (fabs(summand) > epsilon) {
+      summand *= -factor / ((i - 1) * (i - 1) * i * i);
+      i += 2;
+      sum += summand;
+    }
 
-   return sum;
+    return sum;
+  } else {
+    return exp(x / sqrt(2)) / sqrt(2 * PI * x) *
+           (f1(x) * cos(x / sqrt(2) - PI / 8) +
+            g1(x) * sin(x / sqrt(2) - PI / 8));
+  }
 }
 
 double bei(double x) {
-  const double factor = pow(x,4)/16;
+  const double factor = pow(x, 4) / 16;
 
-  double sum = x*x/4;
+  double sum = x * x / 4;
   double summand = sum;
 
   double epsilon = 1E-6;
-  
+
   int i = 3;
   while (fabs(summand) > epsilon) {
     summand *= -factor / ((i - 1) * (i - 1) * i * i);
@@ -59,15 +65,15 @@ double f1(double x) {
   double sum = 1;
   double factor = 1;
   double epsilon = 1E-4;
-  
+
   int k = 1;
-  while(fabs(factor) > epsilon) {
+  while (fabs(factor) > epsilon) {
     factor *= (2 * k - 1) * (2 * k - 1) / (8 * k * x);
-    
+
     sum += cos(k * PI / 4) * factor;
     k++;
   }
-  
+
   return sum;
 }
 
@@ -75,14 +81,14 @@ double g1(double x) {
   double sum = 0;
   double factor = 1;
   double epsilon = 1E-4;
-  
+
   int k = 1;
-  while(fabs(factor) > epsilon) {
+  while (fabs(factor) > epsilon) {
     factor *= (2 * k - 1) * (2 * k - 1) / (8 * k * x);
-    
+
     sum += sin(k * PI / 4) * factor;
     k++;
   }
-  
-  return sum;  
+
+  return sum;
 }
