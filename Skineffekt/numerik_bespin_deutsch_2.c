7c3104a7f724fec53be559dbdc6c9b287c9f4d2b
Christian Bespin <ljchriss@gmail.com>
2014-05-02 17:06:27 +0200

    Testfunktion inkl Minimalbeispiel

--- Skineffekt/numerik_bespin_deutsch_2.c
+++ Skineffekt/numerik_bespin_deutsch_2.c
@@ -103,10 +103,12 @@ void test(double epsilon) {
 	FILE *file = fopen("berbei_min.tsv", "r");
 	double x, ckbei, ckber;
 	
+	printf("x \t \t ber(x) \t bei(x)\t \t ber Lit \t bei Lit \t delta ber \t delta bei\n");
 	while(fscanf(file, "%lf \t %lE \t %lE", &x, &ckber, &ckbei) != EOF) {
-		double delta = fabs((ber(x)-ckber)/ckber);
-		if (delta<epsilon) {
-			printf("%f %lE %lE\n", delta, ckber, ber(x));
+		double deltaber = fabs((ber(x)-ckber)/ckber);
+		double deltabei = fabs((bei(x)-ckbei)/ckbei);
+		if (deltaber<epsilon || deltabei<epsilon) {
+			printf("%f \t %lE \t %lE \t %lE \t %lE \t %f \t %f\n", x, ber(x), bei(x), ckber, ckbei, deltaber, deltabei);
 		}
 	}
 	fclose(file);
