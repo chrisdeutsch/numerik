4bd1818866adcc73fdb57db234d510fca4e4bc5a
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2014-05-10 23:45:10 +0200

    vglswerte

--- Skineffekt/numerik_bespin_deutsch_2.c
+++ Skineffekt/numerik_bespin_deutsch_2.c
@@ -18,11 +18,11 @@ double d_f0(double x);
 double d_g0(double x);
 
 /* Berechnet eine Wertetabelle der Stromverteilung mit N gleichverteilten Werten
-   auf das Intervall 0 bis radius; Einheiten der Parameter im cgs-System */
+   auf das Intervall 0 bis rho_0; Einheiten der Parameter im cgs-System */
 void table(double I_0, double sigma, double mu,
-           double omega, double radius, int N);
+           double omega, double rho_0, int N);
 
-/* vergleicht die Ergebnisse der Funktion dbl func(dbl) mit den idealwerten in
+/* vergleicht die Ergebnisse der Funktion dbl func(dbl) mit den Idealwerten in
    der Datei hinter filename (Format: x-Wert f(x)-Wert). Sollte der relative
    Fehler größer als epsilon sein meldet sich die Funktion */
 void test_func(double (*func)(double), char *filename, double epsilon);
@@ -31,7 +31,7 @@ void test_func(double (*func)(double), char *filename, double epsilon);
 const double kThreshold = 10;
 const double kEpsilon = 1E-6;
 
-/* Vorberechnete Konstanten (pi wegen ansi konformität M_PI nicht immer vorhanden) */
+/* Vorberechnete Konstanten */
 const double kSqrt2 = 1.4142135623730950488016887242097;
 const double kPi = 3.1415926535897932384626433832795;
 
@@ -53,10 +53,14 @@ int main() {
   
   if (choice == 1) {
     printf("\n# Benutze Standard-Werte fuer Kupfer\n");
+    
     printf("# Kreisfrequenz des Wechselstroms in Hz: ");
     scanf("%lf", &omega);
+    omega = fabs(omega);
+    
     printf("# Anzahl der berechneten Werte N: ");
     scanf("%i", &N);
+    N = N < 2 ? 2 : N; /* N muss mind. 2 sein */
     
     table(I_0, sigma, mu, omega, radius, N);
   } else if (choice == 2) {
@@ -64,21 +68,26 @@ int main() {
     
     printf("# Strom I_0 in Fr/s: ");
     scanf("%lf", &I_0);
+    I_0 = fabs(I_0);
     
     printf("# Leitfaehigkeit sigma in 1/s: ");
     scanf("%lf", &sigma);
+    sigma = fabs(sigma);
     
     printf("# Permeabilitaet mu: ");
     scanf("%lf", &mu);
     
     printf("# Leiterradius rho_0 in cm: ");
     scanf("%lf", &radius);
+    radius = fabs(radius);
     
     printf("# Kreisfrequenz des Wechselstroms in Hz: ");
     scanf("%lf", &omega);
+    omega = fabs(omega);
     
     printf("# Anzahl der berechneten Werte N: ");
     scanf("%i", &N);
+    N = N < 2 ? 2 : N; /* N muss mind. 2 sein */
     
     table(I_0, sigma, mu, omega, radius, N);
   } else {
@@ -117,6 +126,7 @@ double ber(double x) {
 double bei(double x) {
   if (fabs(x) < kThreshold) {
     /* Reihendarstellung */
+    /* sum enthält bereits den ersten Summanden daher Start mit k = 1 */
     double sum = x*x / 4;
     double summand = sum;
     double factor = pow(x, 4) / 16;
@@ -142,6 +152,7 @@ double bei(double x) {
 double d_ber(double x) {
   if (fabs(x) < kThreshold) {
     /* Reihendarstellung */
+    /* sum enthält bereits den ersten Summanden daher Start mit k = 2 */
     double sum = -pow(x, 3) / 16;
     double summand = sum;
     double factor = pow(x, 4) / 16;
@@ -176,6 +187,7 @@ double d_ber(double x) {
 double d_bei(double x) {
   if (fabs(x) < kThreshold) {
     /* Reihendarstellung */
+    /* sum enthält bereits den ersten Summanden daher Start mit k = 1 */
     double sum = x / 2;
     double summand = sum;
     const double kFactor = pow(x, 4) / 16;
@@ -240,6 +252,7 @@ double g0(double x) {
 }
 
 double d_f0(double x) {
+  /* sum enthält bereits den ersten Summanden daher Start mit k = 2 */
   double sum = -1/(8 * x * x * kSqrt2);
   double factor = -1/(8 * x * x);
   
@@ -255,6 +268,7 @@ double d_f0(double x) {
 }
 
 double d_g0(double x) {
+  /* sum enthält bereits den ersten Summanden daher Start mit k = 2 */
   double sum = -1/(8 * x * x * kSqrt2);
   double factor = -1/(8 * x * x);
   
@@ -270,33 +284,38 @@ double d_g0(double x) {
 }
 
 void table(double I_0, double sigma, double mu,
-           double omega, double radius, int N) {
-  /* Einheit 1/cm */
+           double omega, double rho_0, int N) {
+  /* Einheit: 1/cm */
   double kappa = 2 * sqrt(kPi * sigma * mu * omega) / 2.99792458E10;
   
   /* Vorberechnete Werte die in in jedem Schleifendurchlauf gleich sind */
-  double factor = I_0 * kappa / (2 * kPi * radius); /* cgs-System Einheit: Fr/s/cm^2 */
-  double denominator = d_ber(kappa*radius) * d_ber(kappa*radius)
-                       + d_bei(kappa*radius) * d_bei(kappa*radius);
+  double factor = I_0 * kappa / (2 * kPi * rho_0);
+  double denominator = d_ber(kappa*rho_0) * d_ber(kappa*rho_0)
+                       + d_bei(kappa*rho_0) * d_bei(kappa*rho_0);
   
   int i;
-  double step = radius / (N - 1);
+  double step = rho_0 / (N - 1);
+  
+  if (fabs(kappa * rho_0) > 1000) {
+    printf("\n\n# WARNUNG: Die implementierten Kelvin-Funktionen wurden nur "
+           "fuer Argumente |x| < 1000 getestet. Es kann zu Ueberlaeufen kommen\n");
+  }
   
   printf("\n# Verwendete Parameter:\n");
   printf("# Strom: I_0 = %E Fr/s\n", I_0);
   printf("# Leitfaehigkeit: sigma = %E 1/s\n", sigma);
   printf("# Permeabilitaet: mu = %E\n", mu);
   printf("# Wechselstromkreisfrequenz: omega = %E 1/s\n", omega);
-  printf("# Leiterradius: rho_0 = %f cm\n\n", radius);
+  printf("# Leiterradius: rho_0 = %f cm\n\n", rho_0);
  
   printf("#rho[cm]\t|j|[Fr/s/cm^2]\t\tphi[rad]\n");
   for(i = 0; i < N; i++) {
     double rho = i*step;
     
-    double real = ( ber(kappa*rho) * d_bei(kappa*radius)
-                    - bei(kappa*rho)* d_ber(kappa*radius) ) / denominator;
-    double imag = ( ber(kappa*rho) * d_ber(kappa*radius)
-                    + bei(kappa*rho) * d_bei(kappa*radius) ) / denominator;
+    double real = ( ber(kappa*rho) * d_bei(kappa*rho_0)
+                    - bei(kappa*rho)* d_ber(kappa*rho_0) ) / denominator;
+    double imag = ( ber(kappa*rho) * d_ber(kappa*rho_0)
+                    + bei(kappa*rho) * d_bei(kappa*rho_0) ) / denominator;
     
     double amplitude = factor * sqrt(real*real + imag*imag);
     double phase = atan2(imag, real);
