acc3c2297deeabb11c0c56cfdcbcfb8bd8f9a0a0
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2014-05-11 12:38:33 +0200

    Finale Version

--- Skineffekt/numerik_bespin_deutsch_2.c
+++ Skineffekt/numerik_bespin_deutsch_2.c
@@ -11,24 +11,29 @@ double d_ber(double x);
 double d_bei(double x);
 
 /* Die folgenden Funktionen werden für die asymptotische Näherung benötigt 
-   vgl. Abramowitz, Stegun (der ker/kei Anteil wurde vernachlässigt) */
+   vgl. Abramowitz, Stegun (der ker/kei Anteil wurde vernachlaessigt) */
 double f0(double x);
 double g0(double x);
 double d_f0(double x);
 double d_g0(double x);
 
 /* Berechnet eine Wertetabelle der Stromverteilung mit N gleichverteilten Werten
-   auf das Intervall 0 bis rho_0; Einheiten der Parameter im cgs-System */
+ * auf das Intervall 0 bis rho_0; Einheiten der Parameter im cgs-System */
 void table(double I_0, double sigma, double mu,
            double omega, double rho_0, int N);
 
 /* vergleicht die Ergebnisse der Funktion dbl func(dbl) mit den Idealwerten in
-   der Datei hinter filename (Format: x-Wert f(x)-Wert). Sollte der relative
-   Fehler größer als epsilon sein meldet sich die Funktion 
-   Benutzung: test_func(ber, "mathematica_vglswerte/ber.tsv", 1E-6); */
+ * der Datei hinter filename (Format: x-Wert f(x)-Wert). Sollte der relative
+ * Fehler groesser als epsilon sein meldet sich die Funktion 
+ * Benutzung: test_func(ber, "mathematica_vglswerte/ber.tsv", 1E-6); */
 void test_func(double (*func)(double), char *filename, double epsilon);
 
-/* Konvergenzeinstellung der Kelvin-Funktionen */
+/* Konvergenzeinstellung der Reihenberechnung für die Kelvin-Funktionen 
+ * kThreshold: für Argumente groesser als 10 wird die asymptotische Näherung
+ *             verwendet 
+ * kEpsilon: ist der letzte Summand der Reihe kleiner als kEpsilon * sum, wobei
+ *           sum die aktuelle Partialsumme ist, wird die Reihenberechnung ab-
+ *           gebrochen */
 const double kThreshold = 10;
 const double kEpsilon = 1E-6;
 
@@ -38,13 +43,14 @@ const double kPi = 3.1415926535897932384626433832795;
 
 int main() {
   /* Standardwerte fuer Kupfer */
-  double I_0 = 1; /* cgs-System Einheit: Fr/s */
-  double sigma = 5.356E+17; /* cgs-System Einheit: 1/s */
-  double omega = 1E+6; /* Einheit: 1/s */
-  double mu = 0.999994; /* Einheit: keine */
-  double radius = 0.1; /* Einheit: cm */
-  int N; /* Anzahl der berechneten Werte */
+  double I_0 = 1;           /* Strom [Fr/s] */
+  double sigma = 5.356E+17; /* Leitfaehigkeit [1/s] */
+  double omega = 1E+6;      /* Kreisfrequenz des Wechselstroms [1/s] */
+  double mu = 0.999994;     /* Permeabilitaet des Leiters [ ] */
+  double rho_0 = 0.1;       /* Radius des Leiters [cm] */
+  int N;                    /* Anzahl der berechneten Werte */
   
+  /* Ein/Ausgabe: die Berechnung findet in der table-Funktion statt */
   int choice;
   printf("# Berechnung der Stromdichteverteilung in einem zylindischen Leiter\n");
   printf("# [1]: Standard-Werte fuer Kupferdraht vom Radius 1mm und Strom I_0 = 1 Fr/s\n");
@@ -63,7 +69,7 @@ int main() {
     scanf("%i", &N);
     N = N < 2 ? 2 : N; /* N muss mind. 2 sein */
     
-    table(I_0, sigma, mu, omega, radius, N);
+    table(I_0, sigma, mu, omega, rho_0, N);
   } else if (choice == 2) {
     printf("\n# benutzerdefinierter Parametersatz\n");
     
@@ -79,8 +85,8 @@ int main() {
     scanf("%lf", &mu);
     
     printf("# Leiterradius rho_0 in cm: ");
-    scanf("%lf", &radius);
-    radius = fabs(radius);
+    scanf("%lf", &rho_0);
+    rho_0 = fabs(rho_0);
     
     printf("# Kreisfrequenz des Wechselstroms in Hz: ");
     scanf("%lf", &omega);
@@ -90,7 +96,7 @@ int main() {
     scanf("%i", &N);
     N = N < 2 ? 2 : N; /* N muss mind. 2 sein */
     
-    table(I_0, sigma, mu, omega, radius, N);
+    table(I_0, sigma, mu, omega, rho_0, N);
   } else {
     printf("Eingabe ungültig\n");
     return 1;
@@ -104,12 +110,13 @@ double ber(double x) {
     /* Reihendarstellung */
     double sum = 1;
     double summand = 1;
-    const double kFactor = pow(x, 4) / 16;
+    double factor = pow(x, 4) / 16;
     
     int k = 1;
     
     while (fabs(summand) > kEpsilon * fabs(sum)) {
-      summand *= -kFactor / ((2*k - 1) * (2*k - 1) * 2*k * 2*k);
+      /* berechnet den naechsten Summanden aus dem Vorigen */
+      summand *= -factor / ((2*k - 1) * (2*k - 1) * 2*k * 2*k);
       sum += summand;
       k++;
     }
@@ -135,6 +142,7 @@ double bei(double x) {
     int k = 1;
     
     while (fabs(summand) > kEpsilon * fabs(sum)) {
+      /* berechnet den naechsten Summanden aus dem Vorigen */
       summand *= -factor / (2*k * 2*k * (2*k + 1) * (2*k + 1));
       sum += summand;
       k++;
@@ -161,6 +169,7 @@ double d_ber(double x) {
     int k = 2;
     
     while (fabs(summand) > kEpsilon * fabs(sum)) {
+      /* berechnet den naechsten Summanden aus dem Vorigen */
       summand *= -factor / ((2*k - 2) * (2*k - 1) * (2*k - 1) * 2*k);
       sum += summand;
       k++;
@@ -191,12 +200,13 @@ double d_bei(double x) {
     /* sum enthält bereits den ersten Summanden daher Start mit k = 1 */
     double sum = x / 2;
     double summand = sum;
-    const double kFactor = pow(x, 4) / 16;
+    double factor = pow(x, 4) / 16;
     
     int k = 1;
     
     while (fabs(summand) > kEpsilon * fabs(sum)) {
-      summand *= -kFactor / ((2*k - 1) * 2*k * 2*k * (2*k + 1));
+      /* berechnet den naechsten Summanden aus dem Vorigen */
+      summand *= -factor / ((2*k - 1) * 2*k * 2*k * (2*k + 1));
       sum += summand;
       k++;
     }
@@ -226,7 +236,7 @@ double f0(double x) {
   
   int k = 1;
   
-  /* factor Vergleich, da Sinus null wird */
+  /* vgl. Abschnitt 2.3.3 in der PDF */
   while (fabs(factor) > kEpsilon * fabs(sum)) {
     factor *= (2*k - 1) * (2*k - 1) / (8 * k * x);
     sum += cos(k * kPi / 4) * factor;
@@ -242,7 +252,7 @@ double g0(double x) {
   
   int k = 1;
   
-  /* factor Vergleich, da Sinus null wird */
+  /* vgl. Abschnitt 2.3.3 in der PDF */
   while (fabs(factor) > kEpsilon * fabs(sum)) {
     factor *= (2*k - 1) * (2*k - 1) / (8 * k * x);
     sum += sin(k * kPi / 4) * factor;
@@ -259,6 +269,7 @@ double d_f0(double x) {
   
   int k = 2;
   
+  /* vgl. Abschnitt 2.3.3 in der PDF */
   while (fabs(factor) > kEpsilon * fabs(sum)) {
     factor *= (2*k - 1) * (2*k - 1) /(8 * (k - 1) * x);
     sum += cos(k * kPi / 4) * factor;
@@ -275,6 +286,7 @@ double d_g0(double x) {
   
   int k = 2;
   
+  /* vgl. Abschnitt 2.3.3 in der PDF */
   while (fabs(factor) > kEpsilon * fabs(sum)) {
     factor *= (2*k - 1) * (2*k - 1) /(8 * (k - 1) * x);
     sum += sin(k * kPi / 4) * factor;
@@ -285,15 +297,18 @@ double d_g0(double x) {
 }
 
 void table(double I_0, double sigma, double mu,
-           double omega, double rho_0, int N) {
-  /* Einheit: 1/cm */
-  double kappa = 2 * sqrt(kPi * sigma * mu * omega) / 2.99792458E10;
-  
+           double omega, double rho_0, int N) { 
   /* Vorberechnete Werte die in in jedem Schleifendurchlauf gleich sind */
+  double kappa = 2 * sqrt(kPi * sigma * mu * omega) / 2.99792458E10;
   double factor = I_0 * kappa / (2 * kPi * rho_0);
-  double denominator = d_ber(kappa*rho_0) * d_ber(kappa*rho_0)
-                       + d_bei(kappa*rho_0) * d_bei(kappa*rho_0);
+
+  /* Die Ableitungen der Kelvin-Funktion haben immer dasselbe Argument: */
+  double d_ber_rho0 = d_ber(kappa * rho_0);
+  double d_bei_rho0 = d_bei(kappa * rho_0);
   
+  double denominator = d_ber_rho0 * d_ber_rho0 + d_bei_rho0 * d_bei_rho0;
+  
+  /* Laufvariable und Schrittgroesse der rho-Werte in der Wertetabelle */
   int i;
   double step = rho_0 / (N - 1);
   
@@ -309,19 +324,24 @@ void table(double I_0, double sigma, double mu,
   printf("# Wechselstromkreisfrequenz: omega = %E 1/s\n", omega);
   printf("# Leiterradius: rho_0 = %f cm\n\n", rho_0);
  
-  printf("#rho[cm]\t|j|[Fr/s/cm^2]\t\tphi[rad]\n");
+  printf("#rho[cm]\t\t|j|[Fr/s/cm^2]\t\tphi[rad]\n");
+  
+  /* Berechnung nach den Formeln 18 - 20 in der PDF */
   for(i = 0; i < N; i++) {
-    double rho = i*step;
+    double rho = i * step;
     
-    double real = ( ber(kappa*rho) * d_bei(kappa*rho_0)
-                    - bei(kappa*rho)* d_ber(kappa*rho_0) ) / denominator;
-    double imag = ( ber(kappa*rho) * d_ber(kappa*rho_0)
-                    + bei(kappa*rho) * d_bei(kappa*rho_0) ) / denominator;
+    /* Mehrfachberechnung von ber/bei vermeiden: */
+    double ber_rho = ber(kappa * rho);
+    double bei_rho = bei(kappa * rho);
+    
+    double real = (ber_rho * d_bei_rho0 - bei_rho * d_ber_rho0) / denominator;
+    
+    double imag = (ber_rho * d_ber_rho0 + bei_rho * d_bei_rho0) / denominator;
     
     double amplitude = factor * sqrt(real*real + imag*imag);
     double phase = atan2(imag, real);
     
-    printf("%.5f\t\t%f\t\t%f\n", i*step, amplitude, phase);
+    printf("%f\t\t%f\t\t%f\n", rho, amplitude, phase);
   }
 }
 
